8cba1df8058edebf8f5453dc3b8e7b368e94d26b
birbi3 <birbie@protonmail.com>
2020-01-23 18:17:17 -0600

    first haskell thing

---
+++ lib_detect.hs
@@ -0,0 +1,20 @@
+import System.IO  
+import Data.List
+
+main = do
+	putStrLn "Enter the file you want to analyze: "
+	file_name <- getLine
+	handle <- openFile file_name ReadMode
+	file <- hGetContents handle
+	let content_data = lines file
+	let content_bool = map (isSubsequenceOf "#include") content_data
+	let _data = zip content_data content_bool
+	let libs = map isLib _data
+	let libs = delete_val "0" libs
+	print(libs)
+
+
+isLib :: (String, Bool) -> String
+isLib (line, val) = if val == True
+	then line
+	else "0"
