a959a2a285bb288ed7006e0c73c4cb0a1f44141b
birbi3 <birbie@protonmail.com>
2020-01-24 14:10:29 -0600

    made better

--- lib_detect.hs
+++ lib_detect.hs
@@ -1,21 +1,30 @@
 import System.IO  
 import Data.List
+import Data.Text.Internal.Search
 
 main = do
+	let someFuncs = ["printf", "strncmp", "strlen"]
 	putStrLn "Enter the file you want to analyze: "
 	file_name <- getLine
 	handle <- openFile file_name ReadMode
 	file <- hGetContents handle
 	let content_data = lines file
-	let content_bool = map (isSubsequenceOf "#include") content_data
-	let _data = zip content_data content_bool
-	let libs = map isLib _data
-	let lib = filter (\x -> x /= "0") libs
+	let lib = checkElm content_data "#include"
+	let funcStuff = map (checkElm content_data) someFuncs
 	print(lib)
+	print(funcStuff)
 
 
 isLib :: (String, Bool) -> String
 isLib (line, val) = if val == True
 	then line
 	else "0"
+
+checkElm :: [String] -> String -> [String]
+checkElm content malFunc = values
+	where 
+		content_bool =  map (isSubsequenceOf malFunc) content
+		_data = zip content content_bool 
+		the_conent = map isLib _data
+		values = filter (\x -> x /= "0") the_conent
 
